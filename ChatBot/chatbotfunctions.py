69278a01e06bfd1f504be8e5d15b9bf145ed6aa8
helgiuniverse <helgiuniverse@gmail.com>
2023-04-20 00:46:37 +0300

    Добавил сохранение вопрос-ответов в json

--- ChatBot/chatbotfunctions.py
+++ ChatBot/chatbotfunctions.py
@@ -1,25 +1,38 @@
 import random
+import json
 
-qa_pairs = {
-    "привет": ["Привет!", "Здравствуйте!", "Рад вас видеть!"],
-    "как дела": ["У меня все хорошо, спасибо за спрос.", "Не могу жаловаться.", "Отлично!"],
-    "что нового": ["Ничего особенного.", "Ничего, что могло бы вас заинтересовать."]
-}
+with open('pairs.json', 'r', encoding='utf-8') as pairs_read:
+    qa_pairs = json.load(pairs_read)
 
 
+# Функция ответа на вопрос
 def get_response(question):
     # Приведение вопроса к нижнему регистру и удаление знаков препинания
     question = question.lower().rstrip("?!")
     # Поиск ответа в словаре qa_pairs
-
     if question in qa_pairs:
         answer = random.choice(qa_pairs[question])
+    elif question == 'cfg':
+        answer = 'Режим отделки'
+    elif question == 'q':
+        answer = 'Завершение работы'
     else:
         answer = "Извините, я не понимаю ваш вопрос."
-
     return answer
 
 
+# Получение доступа к правам одмэна
+def get_god_mod(password):
+    is_admin = False
+    if password == 'kavoisho':
+        is_admin = True
+    return is_admin
+
+
+# Добавление новых пар вопрос - ответ
 def add_new_response(new_question, new_answer):
-    qa_pairs[new_question] = [new_answer]
+    new_question = new_question.lower().rstrip("?!")
+    qa_pairs[new_question] = new_answer.split(',')
+    with open('pairs.json', 'w', encoding='utf-8') as pairs_write:
+        pairs_write.write(json.dumps(qa_pairs))
     print(f'Обновлена конфигурация, на вопрос - {new_question} я буду отвечать - {new_answer}')
--- ChatBot/main.py
+++ ChatBot/main.py
@@ -5,5 +5,14 @@ while is_going:
     user_input = input("Вы: ")
     response = get_response(user_input)
     print("Бот: ", response)
-    if user_input == 'пака':
+    if user_input == 'q':
         is_going = False
+    elif user_input == 'cfg':
+        password = input('Введите пароль администратора: \n')
+        if get_god_mod(password):
+            print('Привет одмэн, введите вопрос, затем ответы через запятую: \n')
+            question = input()
+            answer = input()
+            add_new_response(question, answer)
+        else:
+            print('У вас недостаточно прав')
